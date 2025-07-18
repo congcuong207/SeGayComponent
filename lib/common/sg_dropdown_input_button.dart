0802faf810ba2b97e12984d845fca45de40524e1
ChienPn <ngocchienpnc@gmail.com>
2025-07-19 00:53:43 +0700

    refactor dropdown input button

--- lib/common/sg_dropdown_input_button.dart
+++ lib/common/sg_dropdown_input_button.dart
@@ -33,47 +33,41 @@ class SGDropdownInputButton<T> extends StatefulWidget {
   final String? hintText;
   final TextInputType? inputType;
   final bool enableSearch;
-  // Thêm các thuộc tính mới
   final TextStyle? textStyle;
-  final double? dropdownMaxHeight;
   final double? fontSize;
   final FontWeight? fontWeight;
-  final double? dropdownWidth;
 
   const SGDropdownInputButton({
     super.key,
     required this.controller,
+    required this.items,
+    required this.value,
+    required this.onChanged,
     this.label,
     this.textDataNullSearch,
     this.width,
     this.height,
     this.sizeBorderLine,
-    this.sizeBorderMenuItemLine,
-    this.colorBorder,
     this.sizeBorderCircular,
-    this.colorBorderMenuItem,
+    this.sizeBorderMenuItemLine,
     this.sizeBorderCircularItem,
+    this.colorBorder,
+    this.colorBorderMenuItem,
     this.colorSelectedText,
     this.colorBorderFocus,
     this.colorBorderHover,
     this.colorHoverItem,
     this.isShowSuffixIcon = true,
     this.textAlign,
-    this.contentPadding,
     this.textAlignItem,
-    required this.value,
+    this.contentPadding,
     this.defaultValue,
-    required this.items,
-    required this.onChanged,
     this.hintText,
     this.inputType,
     this.enableSearch = true,
-    // Thêm các tham số mới
     this.textStyle,
-    this.dropdownMaxHeight,
     this.fontSize,
     this.fontWeight,
-    this.dropdownWidth,
   });
 
   @override
@@ -248,108 +242,109 @@ class _SGDropdownInputButtonState<T> extends State<SGDropdownInputButton<T>> {
   }
 
   OverlayEntry _createOverlayEntry() {
-    RenderBox renderBox = context.findRenderObject() as RenderBox;
+    // 获取组件尺寸和位置
+    final RenderBox renderBox = context.findRenderObject() as RenderBox;
     final size = renderBox.size;
+    final RenderObject? overlay = Overlay.of(context).context.findRenderObject();
+    final RenderBox box = renderBox;
+    final Offset position = box.localToGlobal(Offset.zero, ancestor: overlay);
 
-    // Lấy vị trí của widget trong hệ tọa độ toàn cục
-    final RenderObject? overlay =
-        Overlay.of(context).context.findRenderObject();
-    final RenderBox? box = context.findRenderObject() as RenderBox?;
-    final Offset position = box!.localToGlobal(Offset.zero, ancestor: overlay);
-
-    // Tính toán không gian hiện có phía trên và phía dưới
+    // 计算弹出菜单的显示方向和空间
     final screenHeight = MediaQuery.of(context).size.height;
     final spaceAbove = position.dy;
     final spaceBelow = screenHeight - (position.dy + size.height);
-
-    // Chiều cao dự kiến của popup
-    final double estimatedPopupHeight = _filteredItems.isEmpty
-        ? 60 // Chiều cao tối thiểu cho "No Data"
-        : math.min(
-            300,
-            _filteredItems.length *
-                44.0); // 44 là chiều cao ước tính cho mỗi item
-
-    // Xác định hướng hiển thị (true = hiển thị phía trên, false = hiển thị phía dưới)
-    final showAbove =
-        spaceBelow < estimatedPopupHeight && spaceAbove > spaceBelow;
-
+    
+    // 估计弹出菜单高度
+    final itemHeight = 44.0; // 每个选项的估计高度
+    final double estimatedPopupHeight = _filteredItems.isEmpty 
+        ? 60 // "No Data"的最小高度
+        : math.min(300, _filteredItems.length * itemHeight);
+    
+    // 决定显示方向
+    final showAbove = spaceBelow < estimatedPopupHeight && spaceAbove > spaceBelow;
+    
+    // 设置菜单样式和内容
     return OverlayEntry(
       builder: (context) => Positioned(
         width: widget.width ?? size.width,
         child: CompositedTransformFollower(
           link: _layerLink,
           showWhenUnlinked: false,
-          targetAnchor:
-              showAbove ? Alignment.topCenter : Alignment.bottomCenter,
-          followerAnchor:
-              showAbove ? Alignment.bottomCenter : Alignment.topCenter,
-          offset: Offset(0.0, showAbove ? -4 : 4), // Khoảng cách 4px
+          targetAnchor: showAbove ? Alignment.topCenter : Alignment.bottomCenter,
+          followerAnchor: showAbove ? Alignment.bottomCenter : Alignment.topCenter,
+          offset: Offset(0.0, showAbove ? -4 : 4), // 4px间距
           child: Material(
             elevation: 4.0,
-            borderRadius: BorderRadius.circular(widget.sizeBorderCircularItem ??
-                widget.sizeBorderCircular ??
-                12),
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxHeight: 300),
-              child: ListView(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                children: _filteredItems.isNotEmpty
-                    ? _filteredItems.map((item) {
-                        final isSelected =
-                            item.value == (widget.value ?? widget.defaultValue);
-                        Widget child = item.child;
-                        if (child is Text) {
-                          child = Text(
-                            textAlign: widget.textAlignItem ?? TextAlign.center,
-                            (child).data ?? '',
-                            style: TextStyle(
-                              fontSize: widget.fontSize,
-                              color: isSelected
-                                  ? (widget.colorSelectedText ?? Colors.blue)
-                                  : Colors.black,
-                              fontWeight: isSelected
-                                  ? FontWeight.bold
-                                  : FontWeight.normal,
-                            ),
-                          );
-                        }
-                        return MouseRegion(
-                          onEnter: (_) =>
-                              setState(() => _hoveredItem = item.value),
-                          onExit: (_) => setState(() => _hoveredItem = null),
-                          child: Material(
-                            color: Colors.transparent,
-                            child: InkWell(
-                              hoverColor: widget.colorHoverItem ??
-                                  SGAppColors.colorBorderGray.withOpacity(0.15),
-                              onTapDown: (_) {
-                                _onItemSelected(item);
-                              },
-                              child: Container(
-                                padding: widget.width! <= 30
-                                    ? const EdgeInsets.only(top: 5, bottom: 5)
-                                    : const EdgeInsets.symmetric(
-                                        horizontal: 16, vertical: 12),
-                                child: child,
-                              ),
-                            ),
-                          ),
-                        );
-                      }).toList()
-                    : [
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: SGText(
-                            text: widget.textDataNullSearch ?? 'No Data',
-                          ),
-                        ),
-                      ],
-              ),
+            borderRadius: BorderRadius.circular(
+              widget.sizeBorderCircularItem ?? widget.sizeBorderCircular ?? 12
             ),
+            child: _buildDropdownList(),
           ),
         ),
+      ),
+    );
+  }
+  
+  // 构建下拉列表
+  Widget _buildDropdownList() {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 300),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        children: _filteredItems.isNotEmpty
+            ? _filteredItems.map(_buildDropdownItem).toList()
+            : [_buildEmptyView()],
+      ),
+    );
+  }
+
+  // 构建下拉项
+  Widget _buildDropdownItem(DropdownMenuItem<T> item) {
+    final isSelected = item.value == (widget.value ?? widget.defaultValue);
+    
+    // 处理项目中的文本样式
+    Widget child = item.child;
+    if (child is Text) {
+      child = Text(
+        (child).data ?? '',
+        textAlign: widget.textAlignItem ?? TextAlign.center,
+        style: TextStyle(
+          fontSize: widget.fontSize,
+          color: isSelected
+              ? (widget.colorSelectedText ?? Colors.blue)
+              : Colors.black,
+          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+        ),
+      );
+    }
+    
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hoveredItem = item.value),
+      onExit: (_) => setState(() => _hoveredItem = null),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          hoverColor: widget.colorHoverItem ??
+              SGAppColors.colorBorderGray.withOpacity(0.15),
+          onTapDown: (_) => _onItemSelected(item),
+          child: Container(
+            padding: widget.width != null && widget.width! <= 30
+                ? const EdgeInsets.only(top: 5, bottom: 5)
+                : const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+  
+  // 无数据时的视图
+  Widget _buildEmptyView() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SGText(
+        text: widget.textDataNullSearch ?? 'No Data',
       ),
     );
   }
@@ -366,105 +361,124 @@ class _SGDropdownInputButtonState<T> extends State<SGDropdownInputButton<T>> {
 
   @override
   Widget build(BuildContext context) {
-    final effectiveWidth = widget.dropdownWidth ?? widget.width;
-    final effectiveHeight = widget.height;
-
     return CompositedTransformTarget(
       link: _layerLink,
       child: SizedBox(
-        width: effectiveWidth,
-        height: effectiveHeight,
+        width: widget.width,
+        height: widget.height,
         child: TextField(
           controller: widget.controller,
           focusNode: _focusNode,
           readOnly: !widget.enableSearch,
+          enableInteractiveSelection: widget.enableSearch, // 禁用文本选择当搜索功能关闭时
           keyboardType: widget.inputType ?? TextInputType.text,
-          inputFormatters: widget.inputType == TextInputType.number
-              ? [FilteringTextInputFormatter.digitsOnly]
-              : null,
-          textAlign: widget.textAlign ?? TextAlign.end,
-          style: widget.textStyle?.copyWith(
-                fontSize: widget.fontSize,
-                fontWeight: widget.fontWeight,
-              ) ??
-              TextStyle(
-                fontSize: widget.fontSize,
-                fontWeight: widget.fontWeight,
-              ),
-          decoration: InputDecoration(
-            hintText: widget.hintText,
-            hintStyle: TextStyle(
-              fontSize: widget.fontSize,
-              fontWeight: widget.fontWeight,
-            ),
-            border: OutlineInputBorder(
-              borderRadius:
-                  BorderRadius.circular(widget.sizeBorderCircular ?? 12),
-              borderSide: BorderSide(
-                color: widget.colorBorder ?? SGAppColors.colorBorderGray,
-                width: widget.sizeBorderLine ?? 1,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius:
-                  BorderRadius.circular(widget.sizeBorderCircular ?? 12),
-              borderSide: BorderSide(
-                color: widget.colorBorderFocus ?? SGAppColors.info500,
-                width: widget.sizeBorderLine ?? 1,
-              ),
-            ),
-            suffixIcon: (widget.isShowSuffixIcon ?? false)
-                ? (widget.enableSearch && widget.controller.text.isNotEmpty)
-                    ? IconButton(
-                        icon: const Icon(Icons.clear,
-                            color: Colors.grey, size: 18),
-                        onPressed: () {
-                          widget.controller.clear();
-                          if (!_isOpen) _showOverlay();
-                        },
-                      )
-                    : IconButton(
-                        icon: const Icon(Icons.arrow_drop_down),
-                        onPressed: () {
-                          if (_isOpen) {
-                            _removeOverlay();
-                          } else {
-                            _focusNode.requestFocus();
-                            _showOverlay();
-                          }
-                        },
-                      )
-                : null,
-            contentPadding: widget.contentPadding ??
-                const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          ),
-          onTap: () {
-            if (_justSelected) return;
-            _focusNode.requestFocus();
-            if (widget.enableSearch) {
-              widget.controller.clear();
-            }
-            if (!_isOpen) _showOverlay();
-          },
-          onEditingComplete: () {
-            if (widget.enableSearch) {
-              final match = widget.items.firstWhere(
-                (item) =>
-                    (item.child is Text
-                        ? ((item.child as Text).data ?? '')
-                        : item.value.toString()) ==
-                    widget.controller.text,
-                orElse: () =>
-                    DropdownMenuItem<T>(value: null, child: const SizedBox()),
-              );
-              if (match.value == null) {
-                _setControllerTextByValue(_lastSelectedValue);
-              }
-            }
-            _removeOverlay();
-          },
+          inputFormatters: _buildInputFormatters(),
+          textAlign: widget.textAlign ?? TextAlign.center,
+          style: _buildTextStyle(),
+          decoration: _buildInputDecoration(),
+          onTap: _handleTap,
+          onEditingComplete: _handleEditingComplete,
         ),
       ),
     );
+  }
+
+  List<TextInputFormatter>? _buildInputFormatters() {
+    return widget.inputType == TextInputType.number
+        ? [FilteringTextInputFormatter.digitsOnly]
+        : null;
+  }
+
+  TextStyle _buildTextStyle() {
+    return widget.textStyle?.copyWith(
+      fontSize: widget.fontSize,
+      fontWeight: widget.fontWeight,
+    ) ??
+    TextStyle(
+      fontSize: widget.fontSize,
+      fontWeight: widget.fontWeight,
+    );
+  }
+
+  InputDecoration _buildInputDecoration() {
+    return InputDecoration(
+      hintText: widget.hintText,
+      hintStyle: TextStyle(
+        fontSize: widget.fontSize,
+        fontWeight: widget.fontWeight,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(widget.sizeBorderCircular ?? 12),
+        borderSide: BorderSide(
+          color: widget.colorBorder ?? SGAppColors.colorBorderGray,
+          width: widget.sizeBorderLine ?? 1,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(widget.sizeBorderCircular ?? 12),
+        borderSide: BorderSide(
+          color: widget.colorBorderFocus ?? SGAppColors.info500,
+          width: widget.sizeBorderLine ?? 1,
+        ),
+      ),
+      suffixIcon: _buildSuffixIcon(),
+      contentPadding: widget.contentPadding ??
+          const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+    );
+  }
+
+  Widget? _buildSuffixIcon() {
+    if (!(widget.isShowSuffixIcon ?? false)) {
+      return null;
+    }
+    
+    if (widget.enableSearch && widget.controller.text.isNotEmpty) {
+      return IconButton(
+        icon: const Icon(Icons.clear, color: Colors.grey, size: 18),
+        onPressed: () {
+          widget.controller.clear();
+          if (!_isOpen) _showOverlay();
+        },
+      );
+    } else {
+      return IconButton(
+        icon: const Icon(Icons.arrow_drop_down),
+        onPressed: () {
+          if (_isOpen) {
+            _removeOverlay();
+          } else {
+            _focusNode.requestFocus();
+            _showOverlay();
+          }
+        },
+      );
+    }
+  }
+
+  void _handleTap() {
+    if (_justSelected) return;
+    _focusNode.requestFocus();
+    if (widget.enableSearch) {
+      widget.controller.clear();
+    }
+    if (!_isOpen) _showOverlay();
+  }
+
+  void _handleEditingComplete() {
+    if (widget.enableSearch) {
+      final match = widget.items.firstWhere(
+        (item) =>
+            (item.child is Text
+                ? ((item.child as Text).data ?? '')
+                : item.value.toString()) ==
+            widget.controller.text,
+        orElse: () =>
+            DropdownMenuItem<T>(value: null, child: const SizedBox()),
+      );
+      if (match.value == null) {
+        _setControllerTextByValue(_lastSelectedValue);
+      }
+    }
+    _removeOverlay();
   }
 }
