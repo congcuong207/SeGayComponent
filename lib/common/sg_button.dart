d816bebe9ef86856fe2a1c9664902d22290eeb00
congcuong <CuongCC@is.viettel.vn>
2025-07-22 23:18:49 +0700

    update_components_sgbutton

--- lib/common/sg_button.dart
+++ lib/common/sg_button.dart
@@ -1,75 +1,77 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class SGButton extends StatefulWidget {
-  final String text;
-  final VoidCallback? onPressed;
-  final bool outlined;
-  final double borderRadius;
-  final EdgeInsetsGeometry? padding;
-  final bool enabled;
-  final Color? mainColor;
+  class SGButton extends StatefulWidget {
+    final String text;
+    final VoidCallback? onPressed;
+    final bool outlined;
+    final double borderRadius;
+    final EdgeInsetsGeometry? padding;
+    final bool enabled;
+    final Color? mainColor;
+    final FontWeight? fontWeight;
 
-  const SGButton({
-    super.key,
-    required this.text,
-    this.onPressed,
-    this.outlined = false,
-    this.borderRadius = 5,
-    this.padding,
-    this.enabled = true,
-    this.mainColor,
-  });
+    const SGButton({
+      super.key,
+      required this.text,
+      this.onPressed,
+      this.outlined = false,
+      this.borderRadius = 5,
+      this.padding,
+      this.enabled = true,
+      this.mainColor,
+      this.fontWeight,
+    });
 
-  static const Color defaultOrange = Color(0xFFFFA726);
+    static const Color defaultOrange = Color(0xFFFFA726);
 
-  @override
-  State<SGButton> createState() => _SGButtonState();
-}
-
-class _SGButtonState extends State<SGButton> {
-  bool _hovering = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final Color orange = widget.mainColor ?? SGButton.defaultOrange;
-    final isOutlined = widget.outlined || _hovering;
-    final style = isOutlined
-        ? OutlinedButton.styleFrom(
-            side: BorderSide(color: orange, width: 2),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(widget.borderRadius)),
-            padding: widget.padding ?? const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-          )
-        : ElevatedButton.styleFrom(
-            backgroundColor: orange,
-            foregroundColor: Colors.white,
-            elevation: 0,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(widget.borderRadius)),
-            padding: widget.padding ?? const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-          );
-    final child = Text(
-      widget.text,
-      style: TextStyle(
-        color: isOutlined ? orange : Colors.white,
-        fontWeight: FontWeight.w600,
-        fontSize: 18,
-      ),
-    );
-    final button = isOutlined
-        ? OutlinedButton(
-            onPressed: widget.enabled ? widget.onPressed : null,
-            style: style,
-            child: child,
-          )
-        : ElevatedButton(
-            onPressed: widget.enabled ? widget.onPressed : null,
-            style: style,
-            child: child,
-          );
-    return MouseRegion(
-      onEnter: (_) => setState(() => _hovering = true),
-      onExit: (_) => setState(() => _hovering = false),
-      cursor: widget.enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
-      child: button,
-    );
+    @override
+    State<SGButton> createState() => _SGButtonState();
   }
-} 
+
+  class _SGButtonState extends State<SGButton> {
+    bool _hovering = false;
+
+    @override
+    Widget build(BuildContext context) {
+      final Color orange = widget.mainColor ?? SGButton.defaultOrange;
+      final isOutlined = widget.outlined || _hovering;
+      final style = isOutlined
+          ? OutlinedButton.styleFrom(
+              side: BorderSide(color: orange, width: 2),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(widget.borderRadius)),
+              padding: widget.padding ?? const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+            )
+          : ElevatedButton.styleFrom(
+              backgroundColor: orange,
+              foregroundColor: Colors.white,
+              elevation: 0,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(widget.borderRadius)),
+              padding: widget.padding ?? const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+            );
+      final child = Text(
+        widget.text,
+        style: TextStyle(
+          color: isOutlined ? orange : Colors.white,
+          fontWeight: widget.fontWeight,
+          fontSize: 16,
+        ),
+      );
+      final button = isOutlined
+          ? OutlinedButton(
+              onPressed: widget.enabled ? widget.onPressed : null,
+              style: style,
+              child: child,
+            )
+          : ElevatedButton(
+              onPressed: widget.enabled ? widget.onPressed : null,
+              style: style,
+              child: child,
+            );
+      return MouseRegion(
+        onEnter: (_) => setState(() => _hovering = true),
+        onExit: (_) => setState(() => _hovering = false),
+        cursor: widget.enabled ? SystemMouseCursors.click : SystemMouseCursors.basic,
+        child: button,
+      );
+    }
+  } 
